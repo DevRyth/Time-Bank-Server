43220a9fbf9b49c3e3317bbd21d81acac2efc6ca
Abhi088 <abhicoding8@gmail.com>
2021-11-13 00:06:24 +0530

    Added functionality to connect mongodb atlas

--- src/index.js
+++ src/index.js
@@ -6,22 +6,39 @@ const auth = require('./api/auth');
 const app = express();
 app.use(cors());
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/time-bank', {
-    useNewUrlParser: true,
-    // useCreateIndex: false,
-    useUnifiedTopology: true,
-    // useFindAndModify: false,
-})
+// mongodb+srv://dba:<password>@time-bank.kny3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+
+// // 'mongodb://localhost:27017/time-bank'
+
+// mongoose.connect(db_url, {
+//     useNewUrlParser: true,
+//     // useCreateIndex: false,
+//     useUnifiedTopology: true,
+//     // useFindAndModify: false,
+// })
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
+
+const Connection = async () => {
+    const db_url = "mongodb+srv://dba:DevRyth@timebank.kny3u.mongodb.net/TimeBank?retryWrites=true&w=majority";
+    try {
+        await mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true});
+        console.log("Database Connected Successfully");
+    }
+    catch(err) {
+        console.log('Error while connecting to the database ', err)
+    }
+}
+
+Connection();
 
 app.use('/', auth);
 
