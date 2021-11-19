b497f2354cff362438c3ae6d4adf6288bbb089e2
akarshgangwar28 <akarshprog28@gmail.com>
2021-11-19 22:43:43 +0530

    Created Course Schema

---
+++ src/model/course.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const courseSchema=new Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    summary:
+    {
+        type:String,
+        required:true
+    },
+    difficulty:
+    {
+        type:String,
+        required:true
+    },
+    available:{
+        type:boolean
+    },
+    duration:
+    {
+        type: Schema.Types.ObjectId,
+        ref:Duration
+
+    }
+});
