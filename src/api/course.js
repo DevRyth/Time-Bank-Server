919184306393e1c755d84119f7a8dba9b6fab6c5
akarshgangwar28 <akarshprog28@gmail.com>
2021-11-20 23:09:51 +0530

    course.js(API) updated and duration.js updated

--- src/api/course.js
+++ src/api/course.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const Course= require("../model/course");
 const User = require("../model/user");
 
 const router = express.Router();
@@ -9,8 +9,16 @@ router.post("/course",async(req,res)=>{
     const token= wholetoken.slice(0,wholetoken.length/2);
 
     const user = await User.findOne({id:token});
-    if(!user){
+    if(!user)
+    {
         return res.status(409).json({message:"Token Invalid"});
     }
-    
+
+    const course= req.data;
+    const newCourse = new Course(course);
+    await newCourse.save().then(item =>{
+        res.status(200).json(course);
+    }).catch(err => {
+        res.status(400).send("unable to save to database");
+    });
 })
